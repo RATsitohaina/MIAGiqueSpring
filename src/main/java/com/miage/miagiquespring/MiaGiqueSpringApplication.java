9ee8d350c56011cea87c1266584d07531c52ece2
RaTsitohaina <gametsito@gmail.com>
2024-06-06 14:34:56 +0200

    init commit

---
+++ src/main/java/com/miage/miagiquespring/MiaGiqueSpringApplication.java
@@ -0,0 +1,13 @@
+package com.miage.miagiquespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaGiqueSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaGiqueSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miage/miagiquespring/MiaGiqueSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.miagiquespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaGiqueSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
