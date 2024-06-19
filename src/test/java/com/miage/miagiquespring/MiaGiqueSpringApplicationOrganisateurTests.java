86c72b872e2e617d830fa79103f407104ffd1304
RaTsitohaina <gametsito@gmail.com>
2024-06-19 15:34:18 +0200

    [Raz_tsitohaina] MOCK Test Organisateur rename

--- src/test/java/com/miage/miagiquespring/MiaGiqueSpringApplicationOrganisateurTests.java
+++ src/test/java/com/miage/miagiquespring/MiaGiqueSpringApplicationOrganisateurTests.java
@@ -5,8 +5,6 @@ import com.miage.miagiquespring.entities.*;
 import com.miage.miagiquespring.metier.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MiaGiqueSpringApplicationTests {
+class MiaGiqueSpringApplicationOrganisateurTests {
 
     /**
      * logger
